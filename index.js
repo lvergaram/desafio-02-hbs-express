9b88811315a9a7a203098e7f39a61b8e67694901
lvergaram <lvergaramena@gmail.com>
2024-04-20 11:21:45 -0400

    first release vertion

--- index.js
+++ index.js
@@ -4,6 +4,49 @@ import path from 'path'
 
 const app = express()
 
+const services = [
+    {
+      name: "Web Development",
+      description: "We create web applications with the latest technologies.",
+      url: "/services/web-development",
+    },
+    {
+      name: "Mobile Development",
+      description: "We create mobile applications with the latest technologies.",
+      url: "/services/mobile-development",
+    },
+    {
+      name: "DevOps",
+      description: "We create CI/CD pipelines with the latest technologies.",
+      url: "/services/devops",
+    },
+    {
+      name: "QA",
+      description: "We create test cases with the latest technologies.",
+      url: "/services/qa",
+    },
+    {
+      name: "UX/UI",
+      description: "We create designs with the latest technologies.",
+      url: "/services/ux-ui",
+    },
+    {
+      name: "Training",
+      description: "We create training with the latest technologies.",
+      url: "/services/training",
+    },
+    {
+      name: "Consulting",
+      description: "We create consulting with the latest technologies.",
+      url: "/services/consulting",
+    },
+    {
+      name: "Outsourcing",
+      description: "We create outsourcing with the latest technologies.",
+      url: "/services/outsourcing",
+    },
+  ];
+
 // ruta absoluta
 const __dirname = import.meta.dirname
 
@@ -17,24 +60,35 @@ app.set('view engine', '.hbs');
 app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
-    res.render('home', { title: "Home Page 2.0", user: null })
+    res.render('home', { title: "Bienvenido a Mishu Servicios!", user: 'Luis' })
 })
 
-app.get('/about', (req, res) => {
-    res.render('about')
+app.get('/nosotros', (req, res) => {
+    res.render('nosotros')
 })
 
-const products = [
-    { id: 1, title: "ZTE", img: "https://www.pcfactory.cl/public/foto/50037/1_200.jpg?t=1713542184599" },
-    { id: 2, title: "Motorola", img: "https://www.pcfactory.cl/public/foto/48183/1_200.jpg?t=1712348209323" },
-    { id: 3, title: "Motorola", img: "https://www.pcfactory.cl/public/foto/44473/1_200.jpg?t=1709222667989" },
-    { id: 4, title: "Xiaomi", img: "https://www.pcfactory.cl/public/foto/50697/1_200.jpg?t=1712349941174" },
-]
 
-const fruts = ['🍉', '🍓', '🍌']
+app.get('/services', (req, res) => {
+    res.render('services', { services })
+})
 
-app.get('/products', (req, res) => {
-    res.render('products', { products: products, fruts })
+app.get('/services/:param', (req, res) => {
+    const paramValue = req.params.param;
+    const service = services.find( serv => serv.url === `/services/${paramValue}`)
+    if( service){
+        res.render('services-id',{
+            name: service.name,
+            description: service.description
+        })
+    } else {
+        res.status(404).render('404', {message: 'Este servicio no existe o ya no se encuentra'})
+    }
+  });
+
+
+
+app.get('/*', (req,res) => {
+    res.status(404).render('404', {message: 'Esta página no existe'})
 })
 
 const PORT = process.env.PORT || 3000
